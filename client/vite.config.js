8c2592a59130f4103287c598d5df577594ef743a
Abhinav Pandey <118235094+abhinav20git@users.noreply.github.com>
2024-09-15 18:50:38 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port:4001,
     proxy:{
       '/api':{
-        target: 'http://localhost:5001',
+        target: 'https://chat-vercel-server.onrender.com/',
         changeOrigin: true,
       }
     }
