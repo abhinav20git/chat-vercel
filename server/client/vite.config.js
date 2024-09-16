ba9c24bdf2f1cc5f77e9b18101878dee6e454edc
Abhinav Pandey <118235094+abhinav20git@users.noreply.github.com>
2024-09-16 18:43:32 +0530

    Update vite.config.js

--- server/client/vite.config.js
+++ server/client/vite.config.js
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:4001,
-    proxy:{
-      '/api':{
-        target: 'https://chat-vercel-server.onrender.com/',
-        changeOrigin: true,
-      }
-    }
-  }
+  
 })
